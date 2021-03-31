6201d6cb788f1468e554214d274766762f4c5b2a
yinsee <yinsee@3b.my>
2021-03-31 13:09:21 +0800

    fix bug add percentage, marketcap, unit price

--- server/scrapper.php
+++ server/scrapper.php
@@ -1,25 +1,40 @@
 <?php
 header("Access-Control-Allow-Origin: ncat.fun");
 header("Access-Control-Allow-Headers: *");
-deinfe('CACHE_DURATION', 3600);
+define('CACHE_DURATION', 0);
 
 function fetch()
 {
-    throw new Exception("Scrapper Error", 1);
 
     $scrapeTarget = 'https://bscscan.com/token/0x0cF011A946f23a03CeFF92A4632d5f9288c6C70D?a=0x0000000000000000000000000000000000000001';
     $content = file_get_contents($scrapeTarget);
 
     if (preg_match('/Balance<\/h6>\s+([\d,]+)\.\d+\s+NCAT\s+<\/div>/s', $content, $m) &&
-        preg_match('/BscScan shows the price of the Token \$([\d.]+), total supply ([\d,.]+), number of holders ([\d,]+) and updated/s', $content, $n)) {
-        $data = ['balance' => $m[1], 'time' => time(), 'price' => $n[1], 'supply' => $n[2], 'holders' => $n[3]];
+        preg_match('/BscScan shows the price of the Token \$([\d.]+), total supply ([\d,.]+), number of holders ([\d,]+) and updated/s', $content, $n) &&
+        preg_match('/id="pricebutton".*>\s\$([\d,]+)\.\d+\s+<\/span>/s', $content, $p)) {
+        $burn = intval(str_replace(',', '', $m[1]));
+        $total = intval(str_replace(',', '', $n[2]));
+        $holders = intval(str_replace(',', '', $n[3]));
+        $supply = $total - $burn;
+        $marketcap = intval(str_replace(',', '', $p[1]));
+        $data = [
+            'burn' => number_format($burn),
+            'time' => time(),
+            'price' => number_format($marketcap / $supply, 10),
+            'supply' => number_format($supply),
+            'holders' => number_format($holders),
+            'percentage' => number_format($burn / $total * 100, 2),
+            'marketcap' => number_format($marketcap),
+        ];
         return $data;
     }
 
+    throw new Exception("Scrapper Error", 1);
 }
 
 try {
-    $data = @json_decode(@file_get_contents('cache.json'), true) ?? [];
+    $data = [];
+    // @json_decode(@file_get_contents('cache.json'), true) ?? [];
     // if no data or cached expire, refresh
     if (!isset($data['time']) || ($data['time'] + CACHE_DURATION) < time()) {
         $data = fetch();
@@ -28,5 +43,5 @@ try {
     print json_encode($data);
 } catch (Exception $e) {
     // do nothing
-    print json_encode($data);
+    print json_encode(['error' => $e->getMessage()]);
 }
