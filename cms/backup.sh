d8cf91b1cbe27120d17d7cef7cc251cda16e5f66
yinsee <yinsee@3b.my>
2021-05-02 22:20:28 +0800

    delete cms, move to separate project

--- cms/backup.sh
+++
@@ -1,1 +0,0 @@
-rsync -Cavz ncat@149.28.73.165:/var/www/strapi-cms/ .
--- cms/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '5c3371f5416bc86f5c4e56c5ce0d28ed'),
-    },
-  },
-});
