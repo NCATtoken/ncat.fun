d807cb9609abcf17e2f66e573890dc5fd2e15f94
yinsee <yinsee@3b.my>
2021-09-30 01:54:47 +0800

    improve css

--- web/push.sh
+++ web/push.sh
@@ -1,3 +1,5 @@
 ng build --prod
 chmod -fR 755 dist/*
+javascript-obfuscator dist/main-es5*.js --output dist/main-es5*.js --config ./obfuscator.json
+javascript-obfuscator dist/main-es2015*.js --output dist/main-es2015*.js --config ./obfuscator.json
 rsync -crvz dist/* ncat@45.77.122.253:/var/www/web
